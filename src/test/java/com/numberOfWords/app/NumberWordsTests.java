3bc2fb97ecec6cbda6374c4d1ee6e7ae9b10185c
Abdul Khader <khader.m.a7@gmail.com>
2018-10-10 13:40:51 +0530

    initial commit

---
+++ src/test/java/com/numberOfWords/app/NumberWordsTests.java
@@ -0,0 +1,31 @@
+package devops.numbers;
+
+import org.junit.*;
+
+import devops.numbers.NumberWords;
+
+public class NumberWordsTests {
+	private NumberWords numberWords ;
+	
+	@Before
+	public void setup() {
+		numberWords = new NumberWords() ;
+	}
+
+	@Test
+	public void numberOutOfRangeReturnsError() {
+		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords(-1) ) ;
+		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords(200000006) ) ;
+	}
+	@Test
+	public void properNUmber() {
+		Assert.assertEquals( "Success", "Forty Five", numberWords.toWords(45) ) ;
+	}	
+//	@Test
+//	public void exitCommand() {
+//		Assert.assertEquals( "Success", "Exiting the System", numberWords.toWords(0) ) ;
+//	}	
+	
+	
+
+}
